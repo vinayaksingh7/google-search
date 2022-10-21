0da19aa55818b6a2b6eaab00bcc491bf2217e2a0
vinayaksingh7 <77718961+vinayaksingh7@users.noreply.github.com>
2022-10-21 20:22:45 +0530

    Update A.java

--- google-search_local/src/test/java/com/herokuapp/A.java
+++ google-search_local/src/test/java/com/herokuapp/A.java
@@ -1,10 +1,6 @@
-package com.herokuapp;
-
-public class A {
     String name= "rahul";
     String name1= "rahul Savant";
     String name2= "rahul Vinayak";
     String name3= "rahul Sigh";
-
-
-}
+    String name4= "rahul Singh";
+    How are you?
