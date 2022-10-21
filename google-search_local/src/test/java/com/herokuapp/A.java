a35632fd9ec09c83f56cf50d9a262f55210ae144
vinayaksingh7 <77718961+vinayaksingh7@users.noreply.github.com>
2022-10-21 20:26:26 +0530

    Update A.java

--- google-search_local/src/test/java/com/herokuapp/A.java
+++ google-search_local/src/test/java/com/herokuapp/A.java
@@ -4,3 +4,6 @@
     String name3= "rahul Sigh";
     String name4= "rahul Singh";
     How are you?
+    String name3= "Vinayak Singh";
+    String name4= "rahul Savant";
+    How are you, how's everything?
